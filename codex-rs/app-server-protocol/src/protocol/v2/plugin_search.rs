cdc557fe45e26f871b0f523be9fa8d697f34b051
looooonk <cadenhwang05@gmail.com>
2026-09-01 05:46:04 +0900

    refactor: fix plugin search schema imports

--- codex-rs/app-server-protocol/src/protocol/v2/plugin_search.rs
+++ codex-rs/app-server-protocol/src/protocol/v2/plugin_search.rs
@@ -1,9 +1,9 @@
 use super::PluginSummary;
-use crate::JsonSchema;
-use crate::TS;
 use codex_utils_absolute_path::AbsolutePathBuf;
+use schemars::JsonSchema;
 use serde::Deserialize;
 use serde::Serialize;
+use ts_rs::TS;
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, JsonSchema, TS)]
 #[serde(rename_all = "camelCase")]
